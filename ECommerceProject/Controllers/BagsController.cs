ea7d0be704e15b74bf93e13039e0e07dfe35e5c0
Burcu Kocaer <tcburcu.kocaer@gmail.com>
2025-05-27 18:38:51 +0300

    bag,woman,img eklendi

--- ECommerceProject/Controllers/BagsController.cs
+++ ECommerceProject/Controllers/BagsController.cs
@@ -7,14 +7,15 @@ using System.Web.Mvc;
 
 namespace ECommerceProject.Controllers
 {
-    public class WomanClothesController : Controller
+    public class BagsController : Controller
     {
+        // GET: Bags
         private readonly ECommerceProjectContext Db = new ECommerceProjectContext();
 
         public ActionResult Index()
         {
             var products = Db.Products
-                             .Where(p => p.Category.CategoryName == "Kadın")
+                             .Where(p => p.Category.CategoryName == "Çanta")
                              .ToList();
 
             return View(products);
