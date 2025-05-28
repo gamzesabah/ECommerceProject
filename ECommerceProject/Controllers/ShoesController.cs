3d58b8931a338919f13769249a5a443f6408cd3d
gamzesabah <99960350+gamzesabah@users.noreply.github.com>
2025-05-28 21:43:42 +0000

    Ayakkabı tamamlandı

---
+++ ECommerceProject/Controllers/ShoesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ECommerceProject.Context;
+
+namespace ECommerceProject.Controllers
+{
+    public class ShoesController : Controller
+    {
+        private readonly ECommerceProjectContext Db = new ECommerceProjectContext();
+
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            var products = Db.Products
+                             .Where(p => p.Category.CategoryName == "Ayakkabı")
+                             .ToList();
+
+            return View(products);
+        }
+    }
+}
